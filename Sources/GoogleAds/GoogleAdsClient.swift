8dcea8c4adfb8565bad9a2b46e61c0bb3f15896c
zunda.dev@gmail.com <zunda.dev@gmail.com>
2024-12-07 12:43:15 +0900

    fix parameter name

--- Sources/GoogleAds/GoogleAdsClient.swift
+++ Sources/GoogleAds/GoogleAdsClient.swift
@@ -9,7 +9,7 @@ public struct GoogleAdsClient<HTTPClient: HTTPClientProtocol & Sendable>: Sendab
   public var framework: String = "afma-sdk-i-v11.13.0"
   public var isTest: Bool
   public var clientId: String
-  public var area: String
+  public var language: String
   public var userAgent: String
 
   /// Initialize GoogleAds
@@ -17,20 +17,20 @@ public struct GoogleAdsClient<HTTPClient: HTTPClientProtocol & Sendable>: Sendab
   ///   - httpClient: HTTPClient
   ///   - isTest: isTest
   ///   - clientId: Google AdMob ID(Example: ca-app-pub-9106234573254233)
-  ///   - area: Area(Example: ja)
+  ///   - language: Language(Example: ja)
   ///   - nativeVersion: Native Version(Example: 3)
   ///   - userAgent: UserAgent(Example: Mozilla/5.0 (iPhone; CPU iPhone OS 18_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148)
   public init(
     httpClient: HTTPClient,
     isTest: Bool,
     clientId: String,
-    area: String,
+    language: String,
     userAgent: String
   ) {
     self.httpClient = httpClient
     self.isTest = true
     self.clientId = clientId
-    self.area = area
+    self.language = language
     self.userAgent = userAgent
   }
 
@@ -49,7 +49,7 @@ public struct GoogleAdsClient<HTTPClient: HTTPClientProtocol & Sendable>: Sendab
     let queryItems: [URLQueryItem] = [
       .init(name: "adtest", value: self.isTest ? "on" : "off"),
       .init(name: "client", value: self.clientId),
-      .init(name: "hl", value: self.area),
+      .init(name: "hl", value: self.language),
       .init(name: "js", value: self.framework),
       .init(
         name: "sra_urls",
