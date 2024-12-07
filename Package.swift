a96bdb30bc5d5a1680fbaeffb9e14c7b25eca22e
zunda.dev@gmail.com <zunda.dev@gmail.com>
2024-12-07 11:48:29 +0900

    fix dependencies

--- Package.swift
+++ Package.swift
@@ -18,7 +18,6 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.1"),
     .package(url: "https://github.com/zunda-pixel/http-client.git", from: "0.3.0"),
-    .package(url: "https://github.com/zunda-pixel/XMLDocument.git", branch: "fix-error-linux"),
   ],
   targets: [
     .target(
@@ -27,10 +26,6 @@ let package = Package(
         .product(name: "HTTPTypes", package: "swift-http-types"),
         .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
         .product(name: "HTTPClient", package: "http-client"),
-        .product(name: "JebiXML", package: "XMLDocument"),
-      ],
-      swiftSettings: [
-        .interoperabilityMode(.Cxx)
       ]
     ),
     .testTarget(
@@ -39,3 +34,8 @@ let package = Package(
     ),
   ]
 )
+
+#if !os(macOS) && !os(Linux)
+package.dependencies.append(.package(url: "https://github.com/zunda-pixel/XMLDocument.git", branch: "fix-error-linux"))
+package.targets[0].dependencies.append(.product(name: "JebiXML", package: "XMLDocument"))
+#endif
