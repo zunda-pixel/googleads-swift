31df37ca8334c8c2153d8eef1c0391ca49b02d0b
zunda <47569369+zunda-pixel@users.noreply.github.com>
2026-05-05 02:54:23 +0900

    fix: Replace XML Parser library (#47)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
   name: "googleads-swift",
   platforms: [
-    .iOS(.v16),
-    .macOS(.v13),
+    .iOS(.v26),
+    .macOS(.v26),
   ],
   products: [
     .library(
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.1"),
     .package(url: "https://github.com/zunda-pixel/http-client.git", from: "0.3.0"),
-    .package(url: "https://github.com/kewlbear/XMLDocument.git", from: "1.0.0"),
+    .package(url: "https://github.com/compnerd/xylem.git", branch: "main"),
   ],
   targets: [
     .target(
@@ -27,11 +27,8 @@ let package = Package(
         .product(name: "HTTPTypes", package: "swift-http-types"),
         .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
         .product(name: "HTTPClient", package: "http-client"),
-        .product(
-          name: "XMLDocument",
-          package: "XMLDocument",
-          condition: .when(platforms: [.iOS, .tvOS, .watchOS, .visionOS])
-        ),
+        .product(name: "DOMParser", package: "xylem"),
+        .product(name: "XPath", package: "xylem"),
       ]
     ),
     .testTarget(
