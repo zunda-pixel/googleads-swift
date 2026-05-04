02bba8d6149be68c46146e7672c7601ccfa9cb25
zunda <zunda.dev@gmail.com>
2026-05-05 01:01:54 +0900

    replace xml parser library

--- Sources/GoogleAds/Video++.swift
+++ Sources/GoogleAds/Video++.swift
@@ -1,25 +1,16 @@
 import Foundation
-
-#if canImport(XMLDocument)
-  import XMLDocument
-#endif
-
-#if canImport(FoundationXML)
-  import FoundationXML
-#endif
+import DOMParser
+import XPath
+import XMLCore
 
 extension Video {
   init(xmlData: Data) throws {
-    #if canImport(XMLDocument)
-      let document = try XMLDocument(data: xmlData, options: 0)
-    #else
-      let document = try XMLDocument(data: xmlData)
-    #endif
-    let title = try document.nodes(forXPath: "//AdTitle").first?.stringValue
+    let document = try DOMParser.parse(bytes: Array(xmlData).span)
+    let title = try document.firstString(forXPath: "//AdTitle")
 
-    let description = try document.nodes(forXPath: "//Description").first?.stringValue
+    let description = try document.firstString(forXPath: "//Description")
 
-    let durationString = try document.nodes(forXPath: "//Duration").first?.stringValue
+    let durationString = try document.firstString(forXPath: "//Duration")
 
     let durationComponents = try durationString?.split(separator: ":").map {
       if let int = Int($0) {
@@ -49,10 +40,10 @@ extension Video {
       duration = nil
     }
 
-    let advertiser = try document.nodes(forXPath: "//Advertiser").first?.stringValue
+    let advertiser = try document.firstString(forXPath: "//Advertiser")
 
     let impressions = try document.nodes(forXPath: "//Impression").map {
-      if let url = $0.stringValue.flatMap({ URL(string: $0) }) {
+      if let url = URL(string: document.stringValue(of: $0)) {
         return url
       } else {
         throw DecodingError.dataCorrupted(
@@ -63,19 +54,13 @@ extension Video {
       }
     }
     let trackings = try document.nodes(forXPath: "//Tracking").map {
-      guard let element = $0 as? XMLElement else {
+      guard let event = document.attribute(named: "event", of: $0) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Tracking.CodingKeys.event], debugDescription: "event is missing")
         )
       }
 
-      guard let event = element.attribute(forName: "event")?.stringValue else {
-        throw DecodingError.dataCorrupted(
-          .init(codingPath: [Tracking.CodingKeys.event], debugDescription: "event is missing")
-        )
-      }
-
-      guard let url = $0.stringValue.flatMap({ URL(string: $0) }) else {
+      guard let url = URL(string: document.stringValue(of: $0)) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Tracking.CodingKeys.event], debugDescription: "event is missing")
         )
@@ -85,46 +70,35 @@ extension Video {
     }
 
     let medias = try document.nodes(forXPath: "//MediaFile").map {
-      guard let element = $0 as? XMLElement else {
-        throw DecodingError.dataCorrupted(
-          .init(
-            codingPath: [Media.CodingKeys.delivery], debugDescription: "media is can not be decoded"
-          )
-        )
-      }
+      let url = URL(string: document.stringValue(of: $0))
 
-      let url = $0.stringValue.flatMap({ URL(string: $0) })
-
-      guard let delivery = element.attribute(forName: "delivery")?.stringValue else {
+      guard let delivery = document.attribute(named: "delivery", of: $0) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Media.CodingKeys.delivery], debugDescription: "delivery is missing")
         )
       }
-      guard let type = element.attribute(forName: "type")?.stringValue else {
+      guard let type = document.attribute(named: "type", of: $0) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Media.CodingKeys.type], debugDescription: "type is missing")
         )
       }
-      guard let bitrate = element.attribute(forName: "bitrate")?.stringValue.map(Int.init) as? Int
-      else {
+      guard let bitrate = document.attribute(named: "bitrate", of: $0).flatMap(Int.init) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Media.CodingKeys.bitrate], debugDescription: "bitrate is missing")
         )
       }
-      guard let width = element.attribute(forName: "width")?.stringValue.map(Int.init) as? Int
-      else {
+      guard let width = document.attribute(named: "width", of: $0).flatMap(Int.init) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Media.CodingKeys.width], debugDescription: "width is missing")
         )
       }
-      guard let height = element.attribute(forName: "height")?.stringValue.map(Int.init) as? Int
-      else {
+      guard let height = document.attribute(named: "height", of: $0).flatMap(Int.init) else {
         throw DecodingError.dataCorrupted(
           .init(codingPath: [Media.CodingKeys.height], debugDescription: "height is missing")
         )
       }
-      let scalable = element.attribute(forName: "scalable")?.stringValue
-      let maintainAspectRatio = element.attribute(forName: "maintainAspectRatio")?.stringValue
+      let scalable = document.attribute(named: "scalable", of: $0)
+      let maintainAspectRatio = document.attribute(named: "maintainAspectRatio", of: $0)
 
       let jsonDecoder = JSONDecoder()
 
@@ -151,5 +125,54 @@ extension Video {
       trackings: trackings,
       medias: medias
     )
+  }
+}
+
+private extension Document {
+  func nodes(forXPath expression: String) throws -> [Document.Reference] {
+    try XPath.Expression.parse(expression)
+      .nodes(in: self, with: XPath.Context(node: self.root))
+  }
+
+  func firstString(forXPath expression: String) throws -> String? {
+    try nodes(forXPath: expression).first.map { stringValue(of: $0) }
+  }
+
+  func stringValue(of reference: Document.Reference) -> String {
+    let view = self.view(of: reference)
+    switch view.kind {
+    case .document, .element:
+      var string = ""
+      var child = self.firstChild(of: reference)
+      while let current = child {
+        string += stringValue(of: current)
+        child = self.nextSibling(of: current)
+      }
+      return string
+    default:
+      guard let value = view.value else { return "" }
+      return String(decoding: value)
+    }
+  }
+
+  func attribute(named name: String, of reference: Document.Reference) -> String? {
+    var attribute = self.firstAttribute(of: reference)
+    while let current = attribute {
+      let view = self.view(of: current)
+      if let attributeName = view.name,
+        String(decoding: attributeName.local) == name,
+        let value = view.value
+      {
+        return String(decoding: value)
+      }
+      attribute = self.nextAttribute(after: current)
+    }
+    return nil
+  }
+}
+
+private extension String {
+  init(decoding bytes: borrowing Span<XML.Byte>) {
+    self = bytes.withUnsafeBufferPointer { String(decoding: $0, as: UTF8.self) }
   }
 }
